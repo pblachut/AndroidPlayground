b847e621883983c54727179c8ed3400a141311de
unknown <piotrek@piotrek.pl>
2016-04-25 18:37:55 +0200

    fix for fragment

---
+++ TumblrViewer/app/src/main/java/piotrek/tumblrviewer/Events/OpenUrlEvent.java
@@ -0,0 +1,12 @@
+package piotrek.tumblrviewer.Events;
+
+/**
+ * Created by Admin on 2016-04-25.
+ */
+public class OpenUrlEvent {
+    public String url;
+
+    public OpenUrlEvent(String url){
+        this.url = url;
+    }
+}
--- TumblrViewer/app/src/main/java/piotrek/tumblrviewer/PostDetailsFragment.java
+++ TumblrViewer/app/src/main/java/piotrek/tumblrviewer/PostDetailsFragment.java
@@ -27,7 +27,7 @@ public class PostDetailsFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.fragment_post_details ,container, false);
     }
 
     @Override
